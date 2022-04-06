3887117064c037ca00fb3c920b8e076e9837d232
Emmanuel Crown <ayanfecrown@gmail.com>
2022-04-06 12:06:11 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var SimpleReverseProxy = require('simple-reverse-proxy'),
+SimplePathRouter = require('simple-path-router');
+
+new SimplePathRouter()
+.when('/', new SimpleReverseProxy(['http://testing-toolkit.local:8080']))
+.listen(3000);
+
+[8080].forEach(function (port) {
+require('http')
+    .createServer(function (req, res) {
+        res.statusCode = 200;
+        res.end();
+
+        console.log(port);
+    })
+    .listen(port);
+});
