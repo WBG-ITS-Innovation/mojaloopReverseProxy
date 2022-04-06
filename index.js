f6623ec0b7bbdec1617791b83737ebf943cadc3f
Emmanuel Crown <ayanfecrown@gmail.com>
2022-04-06 12:41:55 -0400

    updates

--- index.js
+++ index.js
@@ -5,13 +5,3 @@ new SimplePathRouter()
 .when('/', new SimpleReverseProxy(['http://testing-toolkit.local:8080']))
 .listen(3000);
 
-[3000].forEach(function (port) {
-require('http')
-    .createServer(function (req, res) {
-        res.statusCode = 200;
-        res.end();
-
-        console.log(port);
-    })
-    .listen(port);
-});
